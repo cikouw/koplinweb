1d15e7817d11658026e6daeba6ad80da5b0cf597
cikouw <nathanaelsaruksuk@gmail.com>
2024-06-07 02:23:28 +0700

    ubah ukuran svg

--- src/icons/NasabahSvg.jsx
+++ src/icons/NasabahSvg.jsx
@@ -5,8 +5,8 @@ const NasabahSvg = () => {
     <div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
+        width="35"
+        height="35"
         viewBox="0 0 24 24"
         strokeWidth="1.5"
       >
