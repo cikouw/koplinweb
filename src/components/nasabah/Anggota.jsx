fe4fd422c60e91ceec786c38a7a63cc14e32ffd3
cikouw <nathanaelsaruksuk@gmail.com>
2024-06-08 04:24:42 +0700

    ubah flex

--- src/components/nasabah/Anggota.jsx
+++ src/components/nasabah/Anggota.jsx
@@ -4,12 +4,14 @@ import "./Nasabah.css";
 const Anggota = ({ nama, angsuran, tanggal, alamat, sobekan }) => {
   return (
     <div className="anggota">
-      <div className="flex-nama">
+      <div className="flex-atas">
         <strong>{nama}</strong>
-        <span> {angsuran}</span>
-        <p> {tanggal}</p>
+        <div className="flex-angsuran">
+          <span> {angsuran}</span>
+          <p> {tanggal}</p>
+        </div>
       </div>
-      <div className="flex-alamat">
+      <div className="flex-bawah">
         <span> {alamat}</span>
         <p> {sobekan}</p>
       </div>
